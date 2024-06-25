01e2eed5e011feaf885894c6b17a267404d14a20
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-25 16:38:09 +0200

    feat(bonus1): display large image on thumbnail click

---
+++ bonus/js/script.js
@@ -0,0 +1,60 @@
+console.log("BONUS");
+
+// estrapolo il metodo di creazione app vue
+const { createApp } = Vue;
+
+createApp({
+    // tutti i dati dell'applicazione
+    data () {
+        return {
+            slides: [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, 
+                {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, 
+                {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, 
+                {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, 
+                {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ],
+            currentSlide: 0
+        }
+    },
+    // tutte le funzioni (metodi) della applicazione   
+    methods: {
+        nextSlide (){
+            this.currentSlide++
+            if (this.currentSlide > this.slides.length - 1) {
+                this.currentSlide = 0;
+            }
+        },
+        prevSlide () {
+            this.currentSlide--
+            if (this.currentSlide < 0) {
+                this.currentSlide = this.slides.length - 1;
+            }
+        },
+        setSlide(index) { // cambia l'indice currentSlide in base all'indice della miniatura cliccata.
+            this.currentSlide = index;
+        },
+        
+    }
+
+}).mount('#app');
